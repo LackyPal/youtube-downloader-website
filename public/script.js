19cc736a5a81ed06b6ee7bcecd773ffeb8aa6ed8
lakhindar572@gmail.com <lakhindar572@gmail.com>
2023-06-23 21:18:22 +0530

    update styling

--- public/script.js
+++
@@ -1,9 +0,0 @@
-const htmlEl = document.getElementsByTagName("html")[0];
-
-document.addEventListener("DOMContentLoaded", () => {
-  const theme = window.matchMedia("(prefers-color-scheme: dark)")
-    ? "dark"
-    : "light";
-
-  htmlEl.dataset.theme = theme;
-});
